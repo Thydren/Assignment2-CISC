d1342dc3bc10ffa9f51dc701119f1cbc3877bb58
Gutierrez <brandongutierrez@my.whccd.edu>
2020-01-22 20:59:38 -0800

    Answered 1st

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // 1. Write code to output the following string to the console:
 // "Hello World!"
-
+console.log("Hello World!");
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
 
